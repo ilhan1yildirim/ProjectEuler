1bb916eb3d5d97a1d539b7a5bf962d9edee5ee33
ilhan3 <ilhan1yildirim@gmail.com>
2015-12-11 10:58:33 +0200

    Euler project is moved under github folder
    The Java project including the solutions of the questions (only first 5)
    projecteuler.net is created.

---
+++ Euler/src/com/ilhan/home/Euler.java
@@ -0,0 +1,13 @@
+package com.ilhan.home;
+import com.ilhan.home.EulerSolutions;
+
+public class Euler {
+	public static void main(String[] args){
+		EulerSolutions eulerSolutions = new EulerSolutions();
+		eulerSolutions.Euler1();
+		eulerSolutions.Euler2();
+		eulerSolutions.Euler3();
+		eulerSolutions.Euler4();
+		eulerSolutions.Euler5();
+	}
+}
